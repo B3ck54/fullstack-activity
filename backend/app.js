cf67b8d966adb4692dc7b1040fa7b6a458164769
System Administrator <root@imac-beck.home>
2021-01-09 18:50:04 +0100

    update post + put + delete + get one product

--- backend/app.js
+++ backend/app.js
@@ -26,12 +26,12 @@ app.use(bodyParser.json());
 app.post('/api/products', (req, res, next) => {
     delete req.body._id
     const product = new Product({
-        // ...req.body
-    name: 'Produit test',
-    description: 'ceci est une description',
-    price: 44,
-    inStock: 1,
-    "__v": 0
+        ...req.body
+    // name: 'Produit test',
+    // description: 'ceci est une description',
+    // price: 44,
+    // inStock: 1,
+    // "__v": 0
 });
 product.save()
     .then(product => res.status(200).json({ product }))
@@ -53,18 +53,18 @@ app.get('/api/products/:id', (req, res, next) => {
 });
 
 
-// //Update product
-// app.put('/api/product/:id', (req, res, next) => {
-//     Product.updateOne({ _id: req.params.id }, { ...req,body, _id: req.params.id })
-//     .then(() => res.status(200).json({product}))
-//     .catch(error => res.status(400).json({ error }));
-// });
+//Update product
+app.put('/api/products/:id', (req, res, next) => {
+    Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(product => res.status(200).json({product}))
+    .catch(error => res.status(400).json({ error }));
+});
 
-// //Delete product
-// app.delete('/api/produtct/:id', (req, res, next) => {
-//     Product.deleteOne({ _id: req.params.id })
-//     .then(() => res.status(200).json({ message: 'Produit supprimé !'}))
-//     .catch(error => res.status(400).json({ error }));
-// });  
+//Delete product
+app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Produit supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});  
 
 module.exports = app;
